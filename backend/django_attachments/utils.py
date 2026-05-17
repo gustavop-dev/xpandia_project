0671a525eb01540748b232b4a2f972e21870fb8a
carlos18bp <carlos18bp@gmail.com>
2026-05-17 17:46:40 +0000

    chore: drop unused resized_picture_field util and orphan UserListItem type

--- backend/django_attachments/utils.py
+++ backend/django_attachments/utils.py
@@ -1,11 +1,8 @@
 # -*- coding: utf-8 -*-
 import mimetypes
 import os
-from io import BytesIO
 
-from PIL import Image
 from django.contrib.staticfiles import finders
-from django.core.files.uploadedfile import SimpleUploadedFile
 from django.templatetags.static import static
 
 
@@ -19,29 +16,6 @@ def check_ajax(request):
 		)
 		request._django_attachments_ajax = 'application/json' in accept_types
 	return request._django_attachments_ajax
-
-
-def resized_picture_field(self, image_field, max_image_size=None, force_image_type=None, strip_metadata=False, image_quality=70): # pylint: disable=unused-argument
-	if max_image_size is None and force_image_type is None:
-		return image_field
-	image_file = BytesIO(image_field.read())
-	image = Image.open(image_file).convert('RGB')
-
-	if strip_metadata:
-		image_without_exif = Image.new(image.mode, image.size)
-		image_without_exif.putdata(image.getdata())
-		image_without_exif.format = image.format
-		image = image_without_exif
-
-	if max_image_size is not None:
-		if image.size[0] > max_image_size[0] or image.size[1] > max_image_size[1]:
-			image.thumbnail(max_image_size, Image.ANTIALIAS)
-
-	image_file = BytesIO()
-	image.save(image_file, format=force_image_type or image.format, quality=image_quality)
-	image_field.file = SimpleUploadedFile(image_field.name, image_file.getvalue())
-
-	return image_field
 
 
 def parse_mimetype(filename):
--- frontend/lib/types.ts
+++
@@ -1,7 +0,0 @@
-export type UserListItem = {
-  id: number;
-  email: string;
-  role?: string;
-  is_staff: boolean;
-  is_active: boolean;
-};
